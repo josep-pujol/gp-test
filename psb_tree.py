38a1f4a12f8123815a57eeeaab4a4dd2d16f57a2
josep-pujol <pujol.rabat@gmail.com>
2020-08-13 22:11:06 +0000

    changed variable name value to val in update method PSBTree

--- psb_tree.py
+++ psb_tree.py
@@ -9,9 +9,9 @@ class PSBTree:
         for i, v in enumerate(nums):
             self.update(i, v)
 
-    def update(self, i, value):
-        diff = value - self.nums[i]
-        self.nums[i] = value
+    def update(self, i, val):
+        diff = val - self.nums[i]
+        self.nums[i] = val
         i += 1
         while i < len(self.nums) + 1:
             self.aux[i] += diff
